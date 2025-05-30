0270586c6a3048c0e38087108237b39cfd0858a7
facinick <facinick@gmail.com>
2025-05-30 23:25:26 +0530

    shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import { Button } from "./components/ui/button"
+import { Card } from "./components/ui/card"
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Card>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </Card>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <Card className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Card>
+      <ModeToggle />
+    </ThemeProvider>
   )
 }
 
